245fbb573c3306c996b786c725e95b48845651d8
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2022-05-27 15:55:49 +0530

    Update Prg6-Area_shapes.java

--- Programs/Prg6-Area_shapes.java
+++ Programs/Prg6-Area_shapes.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 class Main{
 	public static void main(String[] args) {
@@ -9,24 +8,15 @@ class Main{
 	        int b=sc.nextInt();
 	        int a=l*b;
 	        System.out.print("Area of rectangle :  "); 
-	        System.out.print(a); 
-	
-	}
-
-}
-
-class Main{
-	public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
+	        System.out.println(a); 
 	        System.out.print("Enter the base : ");
-	        int b = sc.nextInt();
+	        int base = sc.nextInt();
 	        System.out.print("Enter the height : ");
 	        int h=sc.nextInt();
-	        double a=0.5*b*h;
-	        System.out.print("Area of rectangle :  "); 
-	        System.out.print(a); 
+	        double area=0.5*base*h;
+	        System.out.print("Area of triangle :  "); 
+	        System.out.print(area); 
 	
 	}
 
 }
-
